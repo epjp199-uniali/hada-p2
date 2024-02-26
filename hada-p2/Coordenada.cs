249f1985042ddb6573686a22a9e3aac39a015b72
Enrique <epjp1@mscloud.ua.es>
2024-02-26 15:48:22 +0100

    48826316F Control de Excepciones Coordenada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -11,29 +11,92 @@ namespace Hada
         public int Fila;
         public int Columna;
 
-
+        //Constructor por defecto
         public Coordenada()
         {
             Fila = -1;
             Columna = -1;
         }
 
+        //Constructor con parametros int fila, int columna
         public Coordenada(int fila, int columna)
         {
             Fila = fila;
             Columna = columna;
         }
 
+        //Constructor con parametros string fila, string columna
         public Coordenada(string fila, string columna)
         {
             Fila = int.Parse(fila);
             Columna = int.Parse(columna);
         }
 
+        //Constructor de copia
         public Coordenada(Coordenada a)
         {
             Fila = a.Fila;
             Columna = a.Columna;
+        }
+        
+        //Metodo ToString(Define como se muestran los datos por pantalla)
+        public override string ToString()
+        {
+            String output;
+
+            output = "(" + Fila + "," + Columna + ")";
+
+            return output;
+        }
+
+        //Metodo GetHashCode(codigo unico)
+        public override int GetHashCode()
+        {
+            return this.Fila.GetHashCode() ^ this.Columna.GetHashCode();
+        }
+
+        //Metodo de comparacion con un objeto generico
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return Equals((Coordenada)obj);
+        }
+
+        //Metodo de comparacion con un objeto coordenada
+        public bool Equals(Coordenada obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (this.GetHashCode() != obj.GetHashCode())
+            {
+                return false;
+            }
+
+            System.Diagnostics.Debug.Assert(
+            base.GetType() != typeof(object));
+
+            if (!base.Equals(obj))
+            {
+                return false;
+            }
+
+            return ((this.Fila.Equals(obj.Fila)) && (this.Columna.Equals(obj.Columna)));
         }
     }
 }
