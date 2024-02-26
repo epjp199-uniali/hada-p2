7794eb209a85d84b89d8ac234a115b5ba2fcca1e
Eduardo Bravo Carmona <ebc46@mscloud.ua.es>
2024-02-26 16:48:41 +0100

    20527292E Estructura Tablero

--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -12,5 +12,50 @@ namespace Hada
 
         private List<Coordenada> coordenadasDisparadas;
 
+        private List<Coordenada> coordenadasTocadas;
+
+        private List<Barco> barcos;
+
+        private List<Barco> barcosEliminados;
+
+        private Dictionary<Coordenada, string> casillasTablero;
+
+
+        public Tablero(int tamTablero,List<Barco> barcos)
+        {
+
+        }
+
+        private void inicializaCasillasTablero()
+        {
+
+        }
+
+        public void Disparar(Coordenada c)
+        {
+
+        }
+
+        public string DibujarTablero()
+        {
+
+        }
+ 
+        public string ToString()
+        {
+
+        }
+
+        private void cuandoEventoTocado() // Cuando se invoca se actualizan las casillas del tablero y se imprime el siguiente texto: 
+        {                                 // TABLERO: Barco [NOMBRE_BARCO] tocado en Coordenada: [(FILA,COLUMNA)]
+
+
+        }
+
+        private void cuandoEventoHundido() // Cuando se invoca se imprime el texto: 'TABLERO: barco [NOMBRE_BARCO] hundido !!'
+        {
+
+        }
+        public event EventHandler<EventArgs> eventoFinPartida;
     }
 }
